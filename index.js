084963d3e38524d7809f0657df08996fb7d1defe
linshiyan1992 <linshiyan1992@gmail.com>
2023-10-27 20:02:36 +0800

    test

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+// app.get("/test/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "test", "about.html"));
+// });
+
 app.get("/about", (req, res) => {
   res.send("Node-exb means experience builder in wx cloud");
 });
