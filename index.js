ea4c07096acc69d66fc1024232fe93ad76e08066
linshiyan1992 <linshiyan1992@gmail.com>
2023-10-27 21:23:48 +0800

    webapp

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require("path");
 
 const app = express();
 
-app.use("/test", express.static("test"));
+app.use("/cdn/1", express.static("/cdn/1"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/:folder/:fileName", (req, res) => {
-  const { folder, fileName } = req.params;
-  res.sendFile(path.join(__dirname, folder, fileName));
-});
+// app.get("/:folder/:fileName", (req, res) => {
+//   const { folder, fileName } = req.params;
+//   res.sendFile(path.join(__dirname, folder, fileName));
+// });
 
 app.get("/about", (req, res) => {
   res.send("Node-exb means experience builder in wx cloud");
