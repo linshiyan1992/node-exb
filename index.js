96fa2d6f8210fbc3d63108098f73d99412bbe6df
linshiyan1992 <linshiyan1992@gmail.com>
2023-10-27 16:36:50 +0800

    add html file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("欢迎使用微信云托管");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/about", (req, res) => {
   res.send("Node-exb means experience builder in wx cloud");
