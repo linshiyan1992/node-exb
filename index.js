0127b05acd11516ec70763cc934e4c369f884d9d
linshiyan1992 <linshiyan1992@gmail.com>
2023-10-27 21:17:58 +0800

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("test"));
+app.use("/test", express.static("test"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
