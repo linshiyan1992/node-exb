fece70467aac133caf305816269704ac6933dc75
linshiyan1992 <linshiyan1992@gmail.com>
2023-10-27 11:07:02 +0800

    test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("欢迎使用微信云托管");
 });
+app.get("/about", (req, res) => {
+  res.send("Node-exb means experience builder in wx cloud");
+});
 
 const port = process.env.PORT || 80;
 
